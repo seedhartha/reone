c99614d3f5a4940ce7e162c88e0026819b069472
Vsevolod Kremianskii <vkremianskii@gmail.com>
2020-08-10 08:32:24 +0700

    chore: Fix copyright notice in routines files

--- src/script/routines_kotor.cpp
+++ src/script/routines_kotor.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright ©  Vsevolod Kremianskii
+ * Copyright © 2020 Vsevolod Kremianskii
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
