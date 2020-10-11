33bd87fe2d1f44b245a566dc8ae3b96348b46de1
Vsevolod Kremianskii <vkremianskii@gmail.com>
2020-10-11 17:08:28 +0700

    test: Add unit test for ActionQueue

---
+++ tests/actionqueue.cpp
@@ -0,0 +1,47 @@
+/*
+ * Copyright © 2020 Vsevolod Kremianskii
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#define BOOST_TEST_MODULE actionqueue
+
+#include <boost/test/included/unit_test.hpp>
+
+#include "../src/game/actionqueue.h"
+
+using namespace std;
+
+using namespace reone::game;
+
+BOOST_AUTO_TEST_CASE(test_action_completion) {
+    ActionQueue actionQueue;
+    actionQueue.push(make_unique<Action>(ActionType::PauseConversation));
+    actionQueue.push(make_unique<Action>(ActionType::ResumeConversation));
+
+    Action *currentAction = actionQueue.currentAction();
+    BOOST_TEST((currentAction && currentAction->type() == ActionType::PauseConversation));
+
+    currentAction->complete();
+    actionQueue.update();
+
+    currentAction = actionQueue.currentAction();
+    BOOST_TEST((currentAction && currentAction->type() == ActionType::ResumeConversation));
+
+    currentAction->complete();
+    actionQueue.update();
+
+    currentAction = actionQueue.currentAction();
+    BOOST_TEST(!currentAction);
+}
