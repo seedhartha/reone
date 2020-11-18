5175df11d918c7540038a352a057d50bbb71901a
seedhartha <seedhartha.github@gmail.com>
2020-11-18 11:07:58 +0700

    chore: Switch to a centralized copyright notice
    Contributors are listed in a separate CONTRIBUTORS file.

--- src/game/player.h
+++ src/game/player.h
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2020 Vsevolod Kremianskii
+* Copyright (c) 2020 The reone project contributors
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
