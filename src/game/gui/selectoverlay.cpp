2f0850be4a8ebeacefd0446bcbc37097333cc22a
uwadmin12 <phamguam12@gmail.com>
2020-11-22 10:37:54 -0500

    fix: build error.

--- src/game/gui/selectoverlay.cpp
+++ src/game/gui/selectoverlay.cpp
@@ -123,7 +123,7 @@ bool SelectionOverlay::handleMouseButtonDown(const SDL_MouseButtonEvent &event) 
         }
 
         case ContextualAction::Attack: {
-            shared_ptr<Creature> &partyLeader =  _game->party().leader();
+            shared_ptr<Creature> partyLeader(_game->party().leader());
             ActionQueue &actions = partyLeader->actionQueue();
             actions.add(make_unique<AttackAction>(static_pointer_cast<Creature>(object),
                         partyLeader->attackRange()));
