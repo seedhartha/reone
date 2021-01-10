d4184ef3479b55ec2a5f9dfa4c3a8ec20d60de9e
Vsevolod Kremianskii <vkremianskii@gmail.com>
2021-01-10 15:46:12 +0700

    feat: Load emitter controllers from MDL

--- src/render/model/mdlfile.h
+++ src/render/model/mdlfile.h
@@ -44,27 +44,34 @@ private:
 
     void doLoad() override;
     void openMDX();
-    void readArrayDefinition(uint32_t &offset, uint32_t &count);
-    void readNodeNames(const std::vector<uint32_t> &offsets);
-    std::unique_ptr<render::ModelNode> readNode(uint32_t offset, render::ModelNode *parent);
-    void readLight(render::ModelNode &node);
-    std::unique_ptr<render::ModelMesh> readMesh();
-    void readSkin(render::ModelNode &node);
-    void readEmitter(render::ModelNode &node);
-    std::vector<std::unique_ptr<render::Animation>> readAnimations(const std::vector<uint32_t> &offsets);
+
     std::unique_ptr<render::Animation> readAnimation(uint32_t offset);
+    std::unique_ptr<render::ModelMesh> readMesh();
+    std::unique_ptr<render::ModelNode> readNode(uint32_t offset, render::ModelNode *parent);
+    std::vector<std::unique_ptr<render::Animation>> readAnimations(const std::vector<uint32_t> &offsets);
+    void readArrayDefinition(uint32_t &offset, uint32_t &count);
+    void readControllers(uint32_t keyCount, uint32_t keyOffset, const std::vector<float> &data, render::ModelNode &node);
+    void readEmitter(render::ModelNode &node);
+    void readLight(render::ModelNode &node);
+    void readNodeNames(const std::vector<uint32_t> &offsets);
+    void readSkin(render::ModelNode &node);
+
     Model::Classification getClassification(int value) const;
 
     // Controllers
 
-    void readControllers(uint32_t keyCount, uint32_t keyOffset, const std::vector<float> &data, render::ModelNode &node);
-    void readPositionController(uint16_t rowCount, uint8_t columnCount, uint16_t timeIndex, uint16_t dataIndex, const std::vector<float> &data, render::ModelNode &node);
-    void readOrientationController(uint16_t rowCount, uint8_t columnCount, uint16_t timeIndex, uint16_t dataIndex, const std::vector<float> &data, render::ModelNode &node);
-    void readColorController(uint16_t dataIndex, const std::vector<float> &data, render::ModelNode &node);
-    void readSelfIllumColorController(uint16_t dataIndex, const std::vector<float> &data, render::ModelNode &node);
     void readAlphaController(uint16_t dataIndex, const std::vector<float> &data, render::ModelNode &node);
-    void readRadiusController(uint16_t dataIndex, const std::vector<float> &data, render::ModelNode &node);
+    void readBirthrateController(uint16_t dataIndex, const std::vector<float> &data, render::ModelNode &node);
+    void readColorController(uint16_t dataIndex, const std::vector<float> &data, render::ModelNode &node);
+    void readLifeExpectancyController(uint16_t dataIndex, const std::vector<float> &data, render::ModelNode &node);
     void readMultiplierController(uint16_t dataIndex, const std::vector<float> &data, render::ModelNode &node);
+    void readOrientationController(uint16_t rowCount, uint8_t columnCount, uint16_t timeIndex, uint16_t dataIndex, const std::vector<float> &data, render::ModelNode &node);
+    void readPositionController(uint16_t rowCount, uint8_t columnCount, uint16_t timeIndex, uint16_t dataIndex, const std::vector<float> &data, render::ModelNode &node);
+    void readRadiusController(uint16_t dataIndex, const std::vector<float> &data, render::ModelNode &node);
+    void readSelfIllumColorController(uint16_t dataIndex, const std::vector<float> &data, render::ModelNode &node);
+    void readSizeStartController(uint16_t dataIndex, const std::vector<float> &data, render::ModelNode &node);
+    void readSizeXController(uint16_t dataIndex, const std::vector<float> &data, render::ModelNode &node);
+    void readSizeYController(uint16_t dataIndex, const std::vector<float> &data, render::ModelNode &node);
 
     // END Controllers
 };
