bac88938047e1e0dda516e6758cb15b09601b691
seedhartha <seedhartha.github@gmail.com>
2021-01-14 09:50:06 +0700

    chore: Cleanup TpcTool

--- tools/tpctool.cpp
+++ tools/tpctool.cpp
@@ -145,7 +145,7 @@ void TpcTool::convert(const fs::path &path, const fs::path &destPath) const {
     TGA *tga = TGAOpen(const_cast<char *>(tgaPath.string().c_str()), "wb");
     if (!tga) {
         error("TGA: write failed: " + tgaPath.string());
-        goto cleanup;
+        return;
     }
 
     tga->hdr = header;
@@ -156,7 +156,8 @@ void TpcTool::convert(const fs::path &path, const fs::path &destPath) const {
         error("TPC: convert failed: " + msg);
     }
 
-cleanup:
+    // Cleanup
+
     TGAClose(tga);
 }
 
