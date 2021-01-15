3d76075b138047f66c0861c476fc6e7e34cbc6f2
seedhartha <seedhartha.github@gmail.com>
2021-01-16 01:00:42 +0700

    fix: Do not spawn explosion emitter particles

--- src/scene/node/emitternode.cpp
+++ src/scene/node/emitternode.cpp
@@ -60,7 +60,6 @@ void EmitterSceneNode::update(float dt) {
 void EmitterSceneNode::spawnParticles(float dt) {
     switch (_emitter->updateType()) {
         case Emitter::UpdateType::Fountain:
-        case Emitter::UpdateType::Explosion:
             _birthTimer.update(dt);
             if (_birthTimer.hasTimedOut()) {
                 if (_particles.size() < kMaxParticleCount) {
