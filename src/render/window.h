170cca7c59331bb437751c8cff62476bdcbd9e88
Vsevolod Kremianskii <vkremianskii@gmail.com>
2021-01-30 16:01:28 +0700

    chore(render): Drop the global FPS counter

--- src/render/window.h
+++ src/render/window.h
@@ -59,8 +59,7 @@ private:
     SDL_GLContext _context { nullptr };
     bool _relativeMouseMode { false };
     std::shared_ptr<Cursor> _cursor;
-    FpsCounter _fpsLocal;
-    FpsCounter _fpsGlobal;
+    FpsCounter _fps;
     std::function<void()> _onRenderWorld;
     std::function<void()> _onRenderGUI;
 
