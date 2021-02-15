c09cf69189f5187c908f2ec2c6706d4eae7608c3
seedhartha <seedhartha.github@gmail.com>
2021-02-15 18:22:28 +0700

    fix: Increase shadow bias, self-illuminated node do not cast shadows

--- src/render/shaders.cpp
+++ src/render/shaders.cpp
@@ -217,7 +217,7 @@ float getShadow() {
     float currentDepth = length(fragToLight);
 
     float shadow = 0.0;
-    float bias = 0.05;
+    float bias = 0.5;
     float samples = 4.0;
     float offset = 0.1;
 
