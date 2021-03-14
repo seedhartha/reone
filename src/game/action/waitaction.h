ca674673f532ede6b24863a5a27b5d59c2e47e4c
seedhartha <seedhartha.github@gmail.com>
2021-03-14 10:11:33 +0700

    feat: Implement ActionWait

---
+++ src/game/action/waitaction.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2020-2021 The reone project contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "glm/common.hpp"
+
+#include "../../common/timer.h"
+
+#include "action.h"
+
+namespace reone {
+
+namespace game {
+
+class WaitAction : public Action {
+public:
+    WaitAction(float seconds) : Action(ActionType::Wait), _timer(seconds) {
+    }
+
+    /**
+     * Advances an internal timer.
+     *
+     * @return true if timer times out, false otherwise
+     */
+    bool advance(float dt) {
+        return _timer.advance(dt);
+    }
+
+private:
+    Timer _timer;
+};
+
+} // namespace game
+
+} // namespace reone
