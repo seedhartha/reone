5c9f7f6c21cda3c677f25338808ffc26696d296c
Vsevolod Kremianskii <vkremianskii@gmail.com>
2021-04-11 06:52:23 +0700

    Revert to writing individual fields in TlkWriter
    This is slower, but takes endianess into account.

--- src/common/streamwriter.h
+++ src/common/streamwriter.h
@@ -40,15 +40,11 @@ public:
     void putUint32(uint32_t val);
     void putInt32(int32_t val);
     void putInt64(int64_t val);
+    void putFloat(float val);
     void putString(const std::string &str);
     void putCString(const std::string &str);
     void putBytes(const ByteArray &bytes);
     void putBytes(int count, uint8_t val = 0);
-
-    template <class T>
-    void putStruct(const T &val) {
-        _stream->write(reinterpret_cast<const char *>(&val), sizeof(T));
-    }
 
     size_t tell() const;
 
