c2db44a849a2176d632c9a5b1484ab0eecad59b1
seedhartha <seedhartha.github@gmail.com>
2021-04-15 11:53:14 +0700

    Make filelists.py accept both Speaker and VO_ResRef
    This yields roughly twice as much dialog entries.

--- scripts/filelists.py
+++ scripts/filelists.py
@@ -6,15 +6,11 @@ import os
 import random
 import sys
 
-extract_dir = r"D:\OpenKotOR\Extract\TSL"
-wav_dir = r"D:\OpenKotOR\Extract\TSL\voices"
+extract_dir = r"D:\OpenKotOR\Extract\KotOR"
 
 
 if not os.path.exists(extract_dir):
     raise RuntimeError("Extraction directory does not exist")
-
-if not os.path.exists(wav_dir):
-    raise RuntimeError("WAV directory does not exist")
 
 
 def index_or_negative_one(string, substr, beg=0):
@@ -46,29 +42,47 @@ def clear_text(text):
     return text.strip()
 
 
-def get_lines_from_dlg(obj, speaker, tlk_strings, uniq_sound):
-    global wav_dir
+def match_entry(entry_speaker, entry_voresref, speaker, voresref):
+    if entry_speaker:
+        return speaker in entry_speaker
+
+    return voresref in entry_voresref if voresref else False
+
+
+def get_lines_from_dlg(obj, speaker, voresref, tlk_strings, uniq_sounds):
+    global extract_dir
+
+    wav_dir = os.path.join(extract_dir, "voices")
+    if not os.path.exists(wav_dir):
+        return []
 
     lines = []
     if "EntryList|15" in obj:
         for entry in obj["EntryList|15"]:
-            if "Speaker|10" in entry and "VO_ResRef|11" in entry:
-                voresref = entry["VO_ResRef|11"].lower()
+            if "VO_ResRef|11" in entry:
                 textstrref = int(entry["Text|12"].split("|")[0])
                 entry_speaker = entry["Speaker|10"].lower()
-                if voresref and (not voresref in uniq_sound) and textstrref != -1 and speaker in entry_speaker:
-                    text = clear_text(tlk_strings[textstrref][1])
+                entry_voresref = entry["VO_ResRef|11"].lower()
+                if textstrref != -1 and match_entry(entry_speaker, entry_voresref, speaker, voresref) and not entry_voresref in uniq_sounds:
+                    text = clear_text(tlk_strings[textstrref])
                     if text:
-                        wav_filename = os.path.join(wav_dir, voresref + ".wav")
+                        wav_filename = os.path.join(wav_dir, entry_voresref + ".wav")
                         if os.path.exists(wav_filename):
                             lines.append("{}|{}|0\n".format(wav_filename, text))
-                            uniq_sound.add(voresref)
+                            uniq_sounds.add(entry_voresref)
 
     return lines
 
 
-def generate_filelist(extract_dir, speaker):
+def generate_filelist(speaker, voresref):
+    """
+    :param speaker: substring to search for in Speaker field of dialog entries
+    :param voresref: substring to search for in VO_ResRef field of dialog entries
+    """
+    global extract_dir
+
     tlk_strings = dict()
+    uniq_sounds = set()
 
     # Read strings from dialog.tlk into dictionary
     tlk_path = os.path.join(extract_dir, "dialog.tlk.json")
@@ -78,18 +92,16 @@ def generate_filelist(extract_dir, speaker):
             if "strings" in obj:
                 for string in obj["strings"]:
                     strref = int(string["_index"])
-                    soundresref = string["soundResRef"].lower()
                     text = string["text"]
-                    tlk_strings[strref] = (soundresref, text)
+                    tlk_strings[strref] = text
 
     # Extract lines from DLG files
     lines = []
-    uniq_sound = set()
     for f in glob.glob("{}/**".format(extract_dir), recursive=True):
         if f.endswith(".dlg.json"):
             with open(f, "r") as fp:
                 obj = json.load(fp)
-                lines.extend(get_lines_from_dlg(obj, speaker, tlk_strings, uniq_sound))
+                lines.extend(get_lines_from_dlg(obj, speaker, voresref, tlk_strings, uniq_sounds))
 
     # Split lines into training and validation filelists
     random.shuffle(lines)
@@ -104,6 +116,9 @@ def generate_filelist(extract_dir, speaker):
 
 
 if len(sys.argv) > 1:
-    generate_filelist(extract_dir, sys.argv[1])
+    # Interpet first argument as Speaker substring, and second argument as VO_ResRef substring
+    speaker = sys.argv[1]
+    voresref = sys.argv[2] if len(sys.argv) > 2 else None
+    generate_filelist(speaker, voresref)
 else:
     print('Usage: python ttsfilelist.py speaker')
