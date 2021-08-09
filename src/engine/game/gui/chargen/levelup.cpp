725da6b628ef60a75240d5f261e377e72a4ebc0f
Vsevolod Kremianskii <vkremianskii@gmail.com>
2021-08-09 11:20:34 +0700

    Use control binding in LevelUpMenu

--- src/engine/game/gui/chargen/levelup.cpp
+++ src/engine/game/gui/chargen/levelup.cpp
@@ -38,11 +38,31 @@ LevelUpMenu::LevelUpMenu(CharacterGeneration *charGen, Game *game) :
 
 void LevelUpMenu::load() {
     GUI::load();
+    bindControls();
     doSetStep(0);
 
     if (_game->id() == GameID::KotOR) {
-        setControlDiscardColor("LBL_DECORATION", glm::vec3(0.0f, 0.0f, 0.082353f));
+        _binding.lblDecoration->setDiscardColor(glm::vec3(0.0f, 0.0f, 0.082353f));
     }
+}
+
+void LevelUpMenu::bindControls() {
+    _binding.btnStepName1 = getControlPtr<Button>("BTN_STEPNAME1");
+    _binding.btnStepName2 = getControlPtr<Button>("BTN_STEPNAME2");
+    _binding.btnStepName3 = getControlPtr<Button>("BTN_STEPNAME3");
+    _binding.btnStepName4 = getControlPtr<Button>("BTN_STEPNAME4");
+    _binding.btnStepName5 = getControlPtr<Button>("BTN_STEPNAME5");
+    _binding.lbl1 = getControlPtr<Label>("LBL_1");
+    _binding.lbl2 = getControlPtr<Label>("LBL_2");
+    _binding.lbl3 = getControlPtr<Label>("LBL_3");
+    _binding.lbl4 = getControlPtr<Label>("LBL_4");
+    _binding.lbl5 = getControlPtr<Label>("LBL_5");
+    _binding.lblDecoration = getControlPtr<Label>("LBL_DECORATION");
+    _binding.lblNum1 = getControlPtr<Label>("LBL_NUM1");
+    _binding.lblNum2 = getControlPtr<Label>("LBL_NUM2");
+    _binding.lblNum3 = getControlPtr<Label>("LBL_NUM3");
+    _binding.lblNum4 = getControlPtr<Label>("LBL_NUM4");
+    _binding.lblNum5 = getControlPtr<Label>("LBL_NUM5");
 }
 
 void LevelUpMenu::reset() {
@@ -51,15 +71,15 @@ void LevelUpMenu::reset() {
 
     // TODO: feats and Force Powers are not yet implemented
 
-    setControlVisible("LBL_1", _hasAttributes);
-    setControlVisible("LBL_3", false);
-    setControlVisible("LBL_4", false);
-    setControlVisible("LBL_NUM1", _hasAttributes);
-    setControlVisible("LBL_NUM3", false);
-    setControlVisible("LBL_NUM4", false);
-    setControlVisible("BTN_STEPNAME1", _hasAttributes);
-    setControlVisible("BTN_STEPNAME3", false);
-    setControlVisible("BTN_STEPNAME4", false);
+    _binding.lbl1->setVisible(_hasAttributes);
+    _binding.lbl3->setVisible(false);
+    _binding.lbl4->setVisible(false);
+    _binding.lblNum1->setVisible(_hasAttributes);
+    _binding.lblNum3->setVisible(false);
+    _binding.lblNum4->setVisible(false);
+    _binding.btnStepName1->setVisible(_hasAttributes);
+    _binding.btnStepName3->setVisible(false);
+    _binding.btnStepName4->setVisible(false);
 }
 
 void LevelUpMenu::goToNextStep() {
@@ -84,43 +104,43 @@ void LevelUpMenu::setStep(int step) {
 void LevelUpMenu::doSetStep(int step) {
     _step = step;
 
-    setControlFocusable("LBL_1", false);
-    setControlFocusable("LBL_2", false);
-    setControlFocusable("LBL_3", false);
-    setControlFocusable("LBL_4", false);
-    setControlFocusable("LBL_5", false);
-    setControlFocusable("BTN_STEPNAME1", false);
-    setControlFocusable("BTN_STEPNAME2", false);
-    setControlFocusable("BTN_STEPNAME3", false);
-    setControlFocusable("BTN_STEPNAME4", false);
-    setControlFocusable("BTN_STEPNAME5", false);
+    _binding.lbl1->setFocusable(false);
+    _binding.lbl2->setFocusable(false);
+    _binding.lbl3->setFocusable(false);
+    _binding.lbl4->setFocusable(false);
+    _binding.lbl5->setFocusable(false);
+    _binding.btnStepName1->setFocusable(false);
+    _binding.btnStepName2->setFocusable(false);
+    _binding.btnStepName3->setFocusable(false);
+    _binding.btnStepName4->setFocusable(false);
+    _binding.btnStepName5->setFocusable(false);
 
-    setControlDisabled("LBL_1", _step != 0);
-    setControlDisabled("LBL_2", _step != 1);
-    setControlDisabled("LBL_3", _step != 2);
-    setControlDisabled("LBL_4", _step != 3);
-    setControlDisabled("LBL_5", _step != 4);
-    setControlDisabled("BTN_STEPNAME1", _step != 0);
-    setControlDisabled("BTN_STEPNAME2", _step != 1);
-    setControlDisabled("BTN_STEPNAME3", _step != 2);
-    setControlDisabled("BTN_STEPNAME4", _step != 3);
-    setControlDisabled("BTN_STEPNAME5", _step != 4);
+    _binding.lbl1->setDisabled(_step != 0);
+    _binding.lbl2->setDisabled(_step != 1);
+    _binding.lbl3->setDisabled(_step != 2);
+    _binding.lbl4->setDisabled(_step != 3);
+    _binding.lbl5->setDisabled(_step != 4);
+    _binding.btnStepName1->setDisabled(_step != 0);
+    _binding.btnStepName2->setDisabled(_step != 1);
+    _binding.btnStepName3->setDisabled(_step != 2);
+    _binding.btnStepName4->setDisabled(_step != 3);
+    _binding.btnStepName5->setDisabled(_step != 4);
 
-    setControlFocus("LBL_1", _step == 0);
-    setControlFocus("LBL_2", _step == 1);
-    setControlFocus("LBL_3", _step == 2);
-    setControlFocus("LBL_4", _step == 3);
-    setControlFocus("LBL_5", _step == 4);
-    setControlFocus("LBL_NUM1", _step == 0);
-    setControlFocus("LBL_NUM2", _step == 1);
-    setControlFocus("LBL_NUM3", _step == 2);
-    setControlFocus("LBL_NUM4", _step == 3);
-    setControlFocus("LBL_NUM5", _step == 4);
-    setControlFocus("BTN_STEPNAME1", _step == 0);
-    setControlFocus("BTN_STEPNAME2", _step == 1);
-    setControlFocus("BTN_STEPNAME3", _step == 2);
-    setControlFocus("BTN_STEPNAME4", _step == 3);
-    setControlFocus("BTN_STEPNAME5", _step == 4);
+    _binding.lbl1->setFocus(_step == 0);
+    _binding.lbl2->setFocus(_step == 1);
+    _binding.lbl3->setFocus(_step == 2);
+    _binding.lbl4->setFocus(_step == 3);
+    _binding.lbl5->setFocus(_step == 4);
+    _binding.lblNum1->setFocus(_step == 0);
+    _binding.lblNum2->setFocus(_step == 1);
+    _binding.lblNum3->setFocus(_step == 2);
+    _binding.lblNum4->setFocus(_step == 3);
+    _binding.lblNum5->setFocus(_step == 4);
+    _binding.btnStepName1->setFocus(_step == 0);
+    _binding.btnStepName2->setFocus(_step == 1);
+    _binding.btnStepName3->setFocus(_step == 2);
+    _binding.btnStepName4->setFocus(_step == 3);
+    _binding.btnStepName5->setFocus(_step == 4);
 }
 
 void LevelUpMenu::onClick(const std::string &control) {
