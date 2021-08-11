ca84606e470674fe0cff472afdc567b815cdf366
seedhartha <seedhartha.github@gmail.com>
2021-08-11 12:25:14 +0700

    Add UTF-16 to UTF-8 conversion function

---
+++ src/engine/common/unicode.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2020-2021 The reone project contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "unicode.h"
+
+#include <codecvt>
+
+using namespace std;
+
+namespace reone {
+
+string convertUTF16ToUTF8(u16string s) {
+    wstring_convert<codecvt_utf8_utf16<char16_t>, char16_t> conversion;
+    return conversion.to_bytes(s);
+}
+
+} // namespace reone
