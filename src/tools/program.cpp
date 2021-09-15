dfc44f064a982d05472d397c176c61794debc965
Vsevolod Kremianskii <vkremianskii@gmail.com>
2021-09-15 12:10:31 +0700

    Separate declaration from definition for Engine and Program
    That is for the sake of consistency.

--- src/tools/program.cpp
+++ src/tools/program.cpp
@@ -17,13 +17,10 @@
 
 #include "program.h"
 
-#include <boost/program_options.hpp>
-
 #include "../engine/common/pathutil.h"
 #include "../engine/resource/types.h"
 
 #include "tools.h"
-#include "types.h"
 
 using namespace std;
 
@@ -64,123 +61,100 @@ static fs::path getDestination(const po::variables_map &vars) {
     return move(result);
 }
 
-class Program : boost::noncopyable {
-public:
-    Program(int argc, char **argv) : _argc(argc), _argv(argv) {
-    }
+int Program::run() {
+    initOptions();
+    parseOptions();
+    loadOptions();
+    loadTools();
 
-    int run() {
-        initOptions();
-        parseOptions();
-        loadOptions();
-        loadTools();
-
-        switch (_operation) {
-            case Operation::None:
-                cout << _optsCmdLine << endl;
-                break;
-            default: {
-                auto tool = getTool();
-                if (tool) {
-                    tool->invoke(_operation, _target, _gamePath, _destPath);
-                } else {
-                    cout << "Unable to choose a tool for the specified operation" << endl;
-                }
-                break;
+    switch (_operation) {
+        case Operation::None:
+            cout << _optsCmdLine << endl;
+            break;
+        default: {
+            auto tool = getTool();
+            if (tool) {
+                tool->invoke(_operation, _target, _gamePath, _destPath);
+            } else {
+                cout << "Unable to choose a tool for the specified operation" << endl;
             }
-        }
-
-        return 0;
-    }
-
-private:
-    int _argc;
-    char **_argv;
-
-    po::options_description _optsCmdLine { "Usage" };
-    po::variables_map _variables;
-
-    fs::path _gamePath;
-    fs::path _destPath;
-    std::string _target;
-    Operation _operation { Operation::None };
-    vector<shared_ptr<ITool>> _tools;
-
-    void initOptions() {
-        _optsCmdLine.add_options()
-            ("game", po::value<string>(), "path to game directory")
-            ("dest", po::value<string>(), "path to destination directory")
-            ("list", "list file contents")
-            ("extract", "extract file contents")
-            ("unwrap", "unwrap an audio file")
-            ("to-json", "convert 2DA, GFF or TLK file to JSON")
-            ("to-tga", "convert TPC image to TGA")
-            ("to-2da", "convert JSON to 2DA")
-            ("to-gff", "convert JSON to GFF")
-            ("to-rim", "create RIM archive from directory")
-            ("to-erf", "create ERF archive from directory")
-            ("to-mod", "create MOD archive from directory")
-            ("to-pth", "convert ASCII PTH to binary PTH")
-            ("to-ascii", "convert binary PTH to ASCII")
-            ("to-tlk", "convert JSON to TLK")
-            ("to-lip", "convert JSON to LIP")
-            ("target", po::value<string>(), "target name or path to input file");
-    }
-
-    void parseOptions() {
-        po::positional_options_description positional;
-        positional.add("target", 1);
-
-        po::parsed_options parsedCmdLineOpts = po::command_line_parser(_argc, _argv)
-            .options(_optsCmdLine)
-            .positional(positional)
-            .run();
-
-        po::store(parsedCmdLineOpts, _variables);
-        po::notify(_variables);
-    }
-
-    void loadOptions() {
-        _gamePath = _variables.count("game") > 0 ? _variables["game"].as<string>() : fs::current_path();
-        _destPath = getDestination(_variables);
-        _target = _variables.count("target") > 0 ? _variables["target"].as<string>() : "";
-
-        // Determine operation from program options
-        for (auto &operation : g_operations) {
-            if (_variables.count(operation.first)) {
-                _operation = operation.second;
-                break;
-            }
+            break;
         }
     }
 
-    void loadTools() {
-        // Tools are queried in the order of addition, whether they support a
-        // particular operation on a particular file, or not. The first tool
-        // to return true gets chosen.
+    return 0;
+}
 
-        _tools.push_back(make_shared<KeyBifTool>());
-        _tools.push_back(make_shared<ErfTool>());
-        _tools.push_back(make_shared<RimTool>());
-        _tools.push_back(make_shared<TwoDaTool>());
-        _tools.push_back(make_shared<TlkTool>());
-        _tools.push_back(make_shared<LipTool>());
-        _tools.push_back(make_shared<GffTool>());
-        _tools.push_back(make_shared<TpcTool>());
-        _tools.push_back(make_shared<PthTool>());
-        _tools.push_back(make_shared<AudioTool>());
-    }
+void Program::initOptions() {
+    _optsCmdLine.add_options()
+        ("game", po::value<string>(), "path to game directory")
+        ("dest", po::value<string>(), "path to destination directory")
+        ("list", "list file contents")
+        ("extract", "extract file contents")
+        ("unwrap", "unwrap an audio file")
+        ("to-json", "convert 2DA, GFF or TLK file to JSON")
+        ("to-tga", "convert TPC image to TGA")
+        ("to-2da", "convert JSON to 2DA")
+        ("to-gff", "convert JSON to GFF")
+        ("to-rim", "create RIM archive from directory")
+        ("to-erf", "create ERF archive from directory")
+        ("to-mod", "create MOD archive from directory")
+        ("to-pth", "convert ASCII PTH to binary PTH")
+        ("to-ascii", "convert binary PTH to ASCII")
+        ("to-tlk", "convert JSON to TLK")
+        ("to-lip", "convert JSON to LIP")
+        ("target", po::value<string>(), "target name or path to input file");
+}
 
-    shared_ptr<ITool> getTool() const {
-        for (auto &tool : _tools) {
-            if (tool->supports(_operation, _target)) return tool;
+void Program::parseOptions() {
+    po::positional_options_description positional;
+    positional.add("target", 1);
+
+    po::parsed_options parsedCmdLineOpts = po::command_line_parser(_argc, _argv)
+        .options(_optsCmdLine)
+        .positional(positional)
+        .run();
+
+    po::store(parsedCmdLineOpts, _variables);
+    po::notify(_variables);
+}
+
+void Program::loadOptions() {
+    _gamePath = _variables.count("game") > 0 ? _variables["game"].as<string>() : fs::current_path();
+    _destPath = getDestination(_variables);
+    _target = _variables.count("target") > 0 ? _variables["target"].as<string>() : "";
+
+    // Determine operation from program options
+    for (auto &operation : g_operations) {
+        if (_variables.count(operation.first)) {
+            _operation = operation.second;
+            break;
         }
-        return nullptr;
     }
-};
+}
 
-int runProgram(int argc, char **argv) {
-    return Program(argc, argv).run();
+void Program::loadTools() {
+    // Tools are queried in the order of addition, whether they support a
+    // particular operation on a particular file, or not. The first tool
+    // to return true gets chosen.
+
+    _tools.push_back(make_shared<KeyBifTool>());
+    _tools.push_back(make_shared<ErfTool>());
+    _tools.push_back(make_shared<RimTool>());
+    _tools.push_back(make_shared<TwoDaTool>());
+    _tools.push_back(make_shared<TlkTool>());
+    _tools.push_back(make_shared<LipTool>());
+    _tools.push_back(make_shared<GffTool>());
+    _tools.push_back(make_shared<TpcTool>());
+    _tools.push_back(make_shared<PthTool>());
+    _tools.push_back(make_shared<AudioTool>());
+}
+
+shared_ptr<ITool> Program::getTool() const {
+    for (auto &tool : _tools) {
+        if (tool->supports(_operation, _target)) return tool;
+    }
+    return nullptr;
 }
 
 } // namespace reone
