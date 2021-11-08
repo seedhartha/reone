479a839e8b23c9806a852ef4811d3bf76d75ba44
Vsevolod Kremianskii <vkremianskii@gmail.com>
2021-11-08 10:41:59 +0700

    Use a single raycast function inside Walkmesh
    This will later be optimized using AABB tree.

--- src/engine/graphics/walkmesh/walkmesh.cpp
+++ src/engine/graphics/walkmesh/walkmesh.cpp
@@ -23,62 +23,22 @@ namespace reone {
 
 namespace graphics {
 
-bool Walkmesh::raycastWalkableFirst(const glm::vec3 &origin, const glm::vec3 &dir, float &distance, int &material) const {
+const Walkmesh::Face *Walkmesh::raycast(const glm::vec3 &origin, const glm::vec3 &dir, float &outDistance) const {
     glm::vec2 baryPosition(0.0f);
-    float tempDistance = 0.0f;
+    float distance = 0.0f;
 
-    for (auto &face : _walkableFaces) {
+    for (auto &face : _faces) {
         const glm::vec3 &p0 = face.vertices[0];
         const glm::vec3 &p1 = face.vertices[1];
         const glm::vec3 &p2 = face.vertices[2];
 
-        if (glm::intersectRayTriangle(origin, dir, p0, p1, p2, baryPosition, tempDistance) && tempDistance > 0.0f) {
-            distance = tempDistance;
-            material = static_cast<int>(face.material);
-            return true;
+        if (glm::intersectRayTriangle(origin, dir, p0, p1, p2, baryPosition, distance) && distance > 0.0f) {
+            outDistance = distance;
+            return &face;
         }
     }
 
-    return false;
-}
-
-bool Walkmesh::raycastNonWalkableFirst(const glm::vec3 &origin, const glm::vec3 &dir, float &distance, glm::vec3 &normal) const {
-    glm::vec2 baryPosition(0.0f);
-    float tempDistance = 0.0f;
-
-    for (auto &face : _nonWalkableFaces) {
-        const glm::vec3 &p0 = face.vertices[0];
-        const glm::vec3 &p1 = face.vertices[1];
-        const glm::vec3 &p2 = face.vertices[2];
-
-        if (glm::intersectRayTriangle(origin, dir, p0, p1, p2, baryPosition, tempDistance) && tempDistance > 0.0f) {
-            distance = tempDistance;
-            normal = face.normal;
-            return true;
-        }
-    }
-
-    return false;
-}
-
-bool Walkmesh::raycastNonWalkableClosest(const glm::vec3 &origin, const glm::vec3 &dir, float &distance, glm::vec3 &normal) const {
-    distance = numeric_limits<float>::max();
-
-    glm::vec2 baryPosition(0.0f);
-    float tempDistance = 0.0f;
-
-    for (auto &face : _nonWalkableFaces) {
-        const glm::vec3 &p0 = face.vertices[0];
-        const glm::vec3 &p1 = face.vertices[1];
-        const glm::vec3 &p2 = face.vertices[2];
-
-        if (glm::intersectRayTriangle(origin, dir, p0, p1, p2, baryPosition, tempDistance) && tempDistance > 0.0f && tempDistance < distance) {
-            distance = tempDistance;
-            normal = face.normal;
-        }
-    }
-
-    return distance != numeric_limits<float>::max();
+    return nullptr;
 }
 
 } // namespace graphics
