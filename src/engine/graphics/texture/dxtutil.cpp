750337b79e024855779e323ebaeae6cd42b3af30
seedhartha <seedhartha.github@gmail.com>
2021-11-23 09:43:05 +0700

    Cleanup DXT decompression code
    - Drop redundant casts
    - Use 64-bit number for reading alpha LUT in DXT5

--- src/engine/graphics/texture/dxtutil.cpp
+++ src/engine/graphics/texture/dxtutil.cpp
@@ -26,34 +26,34 @@ static uint32_t packRGBA(uint8_t r, uint8_t g, uint8_t b, uint8_t a) {
 }
 
 static void decompressDXT1Block(uint32_t x, uint32_t y, uint32_t width, const uint8_t *blockStorage, uint32_t *image) {
-    uint16_t color0 = *reinterpret_cast<const uint16_t *>(blockStorage);
-    uint16_t color1 = *reinterpret_cast<const uint16_t *>(blockStorage + 2);
+    uint16_t color0 = *(blockStorage + 0);
+    uint16_t color1 = *(blockStorage + 2);
+    uint32_t colorCodes = *reinterpret_cast<const uint32_t *>(blockStorage + 4);
 
     uint32_t temp;
 
     temp = (color0 >> 11) * 255 + 16;
-    uint8_t r0 = (uint8_t)((temp / 32 + temp) / 32);
+    uint8_t r0 = static_cast<uint8_t>((temp / 32 + temp) / 32);
     temp = ((color0 & 0x07e0) >> 5) * 255 + 32;
-    uint8_t g0 = (uint8_t)((temp / 64 + temp) / 64);
+    uint8_t g0 = static_cast<uint8_t>((temp / 64 + temp) / 64);
     temp = (color0 & 0x001f) * 255 + 16;
-    uint8_t b0 = (uint8_t)((temp / 32 + temp) / 32);
+    uint8_t b0 = static_cast<uint8_t>((temp / 32 + temp) / 32);
 
     temp = (color1 >> 11) * 255 + 16;
-    uint8_t r1 = (uint8_t)((temp / 32 + temp) / 32);
+    uint8_t r1 = static_cast<uint8_t>((temp / 32 + temp) / 32);
     temp = ((color1 & 0x07e0) >> 5) * 255 + 32;
-    uint8_t g1 = (uint8_t)((temp / 64 + temp) / 64);
+    uint8_t g1 = static_cast<uint8_t>((temp / 64 + temp) / 64);
     temp = (color1 & 0x001f) * 255 + 16;
-    uint8_t b1 = (uint8_t)((temp / 32 + temp) / 32);
-
-    uint32_t code = *reinterpret_cast<const uint32_t *>(blockStorage + 4);
+    uint8_t b1 = static_cast<uint8_t>((temp / 32 + temp) / 32);
 
     for (int j = 0; j < 4; j++) {
         for (int i = 0; i < 4; i++) {
+            int colorCodeIdx = 2 * (4 * j + i);
+            uint8_t colorCode = (colorCodes >> colorCodeIdx) & 0x03;
             uint32_t finalColor = 0;
-            uint8_t positionCode = (code >> 2 * (4 * j + i)) & 0x03;
 
             if (color0 > color1) {
-                switch (positionCode) {
+                switch (colorCode) {
                 case 0:
                     finalColor = packRGBA(r0, g0, b0, 255);
                     break;
@@ -68,7 +68,7 @@ static void decompressDXT1Block(uint32_t x, uint32_t y, uint32_t width, const ui
                     break;
                 }
             } else {
-                switch (positionCode) {
+                switch (colorCode) {
                 case 0:
                     finalColor = packRGBA(r0, g0, b0, 255);
                     break;
@@ -106,48 +106,34 @@ void decompressDXT1(uint32_t width, uint32_t height, const uint8_t *blockStorage
 }
 
 static void decompressDXT5Block(uint32_t x, uint32_t y, uint32_t width, const uint8_t *blockStorage, uint32_t *image) {
-    uint8_t alpha0 = *reinterpret_cast<const uint8_t *>(blockStorage);
-    uint8_t alpha1 = *reinterpret_cast<const uint8_t *>(blockStorage + 1);
-
-    const uint8_t *bits = blockStorage + 2;
-    uint32_t alphaCode1 = bits[2] | (bits[3] << 8) | (bits[4] << 16) | (bits[5] << 24);
-    uint16_t alphaCode2 = bits[0] | (bits[1] << 8);
+    uint8_t alpha0 = *(blockStorage + 0);
+    uint8_t alpha1 = *(blockStorage + 1);
+    uint64_t alphaCodes = *(blockStorage + 2);
 
     uint16_t color0 = *reinterpret_cast<const uint16_t *>(blockStorage + 8);
     uint16_t color1 = *reinterpret_cast<const uint16_t *>(blockStorage + 10);
+    uint32_t colorCodes = *reinterpret_cast<const uint32_t *>(blockStorage + 12);
 
     uint32_t temp;
 
     temp = (color0 >> 11) * 255 + 16;
-    uint8_t r0 = (uint8_t)((temp / 32 + temp) / 32);
+    uint8_t r0 = static_cast<uint8_t>((temp / 32 + temp) / 32);
     temp = ((color0 & 0x07e0) >> 5) * 255 + 32;
-    uint8_t g0 = (uint8_t)((temp / 64 + temp) / 64);
+    uint8_t g0 = static_cast<uint8_t>((temp / 64 + temp) / 64);
     temp = (color0 & 0x001f) * 255 + 16;
-    uint8_t b0 = (uint8_t)((temp / 32 + temp) / 32);
+    uint8_t b0 = static_cast<uint8_t>((temp / 32 + temp) / 32);
 
     temp = (color1 >> 11) * 255 + 16;
-    uint8_t r1 = (uint8_t)((temp / 32 + temp) / 32);
+    uint8_t r1 = static_cast<uint8_t>((temp / 32 + temp) / 32);
     temp = ((color1 & 0x07e0) >> 5) * 255 + 32;
-    uint8_t g1 = (uint8_t)((temp / 64 + temp) / 64);
+    uint8_t g1 = static_cast<uint8_t>((temp / 64 + temp) / 64);
     temp = (color1 & 0x001f) * 255 + 16;
-    uint8_t b1 = (uint8_t)((temp / 32 + temp) / 32);
-
-    uint32_t code = *reinterpret_cast<const uint32_t *>(blockStorage + 12);
+    uint8_t b1 = static_cast<uint8_t>((temp / 32 + temp) / 32);
 
     for (int j = 0; j < 4; j++) {
         for (int i = 0; i < 4; i++) {
-            int alphaCodeIndex = 3 * (4 * j + i);
-            int alphaCode;
-
-            if (alphaCodeIndex <= 12) {
-                alphaCode = (alphaCode2 >> alphaCodeIndex) & 0x07;
-            } else if (alphaCodeIndex == 15) {
-                alphaCode = (alphaCode2 >> 15) | ((alphaCode1 << 1) & 0x06);
-            } else // alphaCodeIndex >= 18 && alphaCodeIndex <= 45
-            {
-                alphaCode = (alphaCode1 >> (alphaCodeIndex - 16)) & 0x07;
-            }
-
+            int alphaCodeIdx = 3 * (4 * j + i);
+            int alphaCode = (alphaCodes >> alphaCodeIdx) & 0x07;
             uint8_t finalAlpha;
             if (alphaCode == 0) {
                 finalAlpha = alpha0;
@@ -166,8 +152,8 @@ static void decompressDXT5Block(uint32_t x, uint32_t y, uint32_t width, const ui
                 }
             }
 
-            uint8_t colorCode = (code >> 2 * (4 * j + i)) & 0x03;
-
+            int colorCodeIdx = 2 * (4 * j + i);
+            uint8_t colorCode = (colorCodes >> colorCodeIdx) & 0x03;
             uint32_t finalColor;
             switch (colorCode) {
             case 0:
