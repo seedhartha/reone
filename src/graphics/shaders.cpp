b4303012158e3453cb05b7da65df123ac851c45e
Vsevolod Kremianskii <vkremianskii@gmail.com>
2022-01-08 10:05:47 +0700

    Improve hashed alpha test stability
    Based on research paper by NVIDIA.

--- src/graphics/shaders.cpp
+++ src/graphics/shaders.cpp
@@ -164,22 +164,25 @@ float hash(vec2 p) {
     return fract(1.0e4 * sin(17.0 * p.x + 0.1 * p.y) * (0.1 + abs(sin(13.0 * p.y + p.x))));
 }
 
-float hash(vec3 p) {
-    return hash(vec2(hash(p.xy), p.z));
-}
-
 void hashedAlphaTest(float a, vec2 p) {
-    float pixDeriv = max(length(dFdx(p)), length(dFdy(p)));
-    float pixScale = 1.0 / pixDeriv;
-    if (a < hash(floor(pixScale * p))) {
-        discard;
-    }
-}
-
-void hashedAlphaTest(float a, vec3 p) {
-    float pixDeriv = max(length(dFdx(p.xy)), length(dFdy(p.xy)));
-    float pixScale = 1.0 / pixDeriv;
-    if (a < hash(floor(pixScale * p))) {
+    float maxDeriv = max(length(dFdx(p)), length(dFdy(p)));
+    float pixScale = 1.0 / maxDeriv;
+    vec2 pixScales = vec2(
+        exp2(floor(log2(pixScale))),
+        exp2(ceil(log2(pixScale))));
+    vec2 alpha = vec2(
+        hash(floor(pixScales.x * p)),
+        hash(floor(pixScales.y * p)));
+    float lerpFactor = fract(log2(pixScale));
+    float x = (1.0 - lerpFactor) * alpha.x + lerpFactor * alpha.y;
+    float t = min(lerpFactor, 1.0 - lerpFactor);
+    vec3 cases = vec3(
+        x * x / (2.0 * t * (1.0 - t)),
+        (x - 0.5 * t) / (1.0 - t),
+        1.0 - (1.0 - x) * (1.0 - x) / (2.0 * t * (1.0 - t)));
+    float threshold = (x < 1.0 - t) ? ((x < t) ? cases.x : cases.y) : cases.z;
+    threshold = clamp(threshold, 1.0e-6, 1.0);
+    if (a < threshold) {
         discard;
     }
 }
