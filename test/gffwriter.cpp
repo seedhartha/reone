2ff7ceb14e61161adfdcbd32b9e0fc496d18f943
seedhartha <seedhartha.github@gmail.com>
2022-04-25 11:04:59 +0700

    Add TwoDaReader unit test

---
+++ test/gffwriter.cpp
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2020-2021 The reone project contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <boost/test/unit_test.hpp>
+
+#include "../src/common/streamwriter.h"
+#include "../src/resource/format/gffwriter.h"
+
+using namespace std;
+
+using namespace reone;
+using namespace reone::resource;
+
+BOOST_AUTO_TEST_SUITE(gff_writer)
+
+BOOST_AUTO_TEST_CASE(should) {
+}
+
+BOOST_AUTO_TEST_SUITE_END()
