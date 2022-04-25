d893142b2902be8cbd60e4f2601318f54ff7d746
Vsevolod Kremianskii <vkremianskii@gmail.com>
2022-04-25 11:04:59 +0700

    Add TwoDaReader unit test

---
+++ test/2dareader.cpp
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2020-2021 The reone project contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <boost/test/unit_test.hpp>
+
+#include "../src/common/streamwriter.h"
+#include "../src/resource/2da.h"
+#include "../src/resource/format/2dareader.h"
+
+using namespace std;
+
+using namespace reone;
+using namespace reone::resource;
+
+BOOST_AUTO_TEST_SUITE(two_da_reader)
+
+BOOST_AUTO_TEST_CASE(should_read_two_da) {
+    // given
+    auto stream = make_shared<istringstream>(string("2DA V2.b\x0akey\x09value\x09\x00\x01\x00\x00\x00\x30\x09\x00\x00\x02\x00\x04\x00\x40\x00\x41\x00", 36));
+    auto reader = TwoDaReader();
+
+    // when
+    reader.load(stream);
+
+    // then
+    auto twoDa = reader.twoDa();
+    BOOST_CHECK_EQUAL(twoDa->getColumnCount(), 2);
+    BOOST_CHECK_EQUAL(twoDa->getRowCount(), 1);
+    BOOST_CHECK_EQUAL(string("@", 1), twoDa->getString(0, "key"));
+    BOOST_CHECK_EQUAL(string("A", 1), twoDa->getString(0, "value"));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
