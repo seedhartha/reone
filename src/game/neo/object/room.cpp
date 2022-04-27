106c15388086780f854e226caf76b021e7c2eaa1
Vsevolod Kremianskii <vkremianskii@gmail.com>
2022-04-27 18:05:14 +0700

    neo: Introduce Room::Loader

--- src/game/neo/object/room.cpp
+++ src/game/neo/object/room.cpp
@@ -17,11 +17,39 @@
 
 #include "room.h"
 
+#include "../../../graphics/models.h"
+#include "../../../graphics/services.h"
+#include "../../../scene/graph.h"
+#include "../../../scene/graphs.h"
+#include "../../../scene/node/model.h"
+#include "../../../scene/services.h"
+
+#include "../../services.h"
+
+using namespace std;
+
+using namespace reone::scene;
+
 namespace reone {
 
 namespace game {
 
 namespace neo {
+
+unique_ptr<Room> Room::Loader::load(const string &name, const glm::vec3 &position) {
+    auto &scene = _services.scene.graphs.get(kSceneMain);
+    auto model = _services.graphics.models.get(name);
+    auto sceneNode = scene.newModel(move(model), ModelUsage::Room, nullptr);
+
+    auto transform = glm::translate(position);
+    sceneNode->setLocalTransform(move(transform));
+
+    return Room::Builder()
+        .id(_idSeq.nextObjectId())
+        .tag(name)
+        .sceneNode(move(sceneNode))
+        .build();
+}
 
 } // namespace neo
 
