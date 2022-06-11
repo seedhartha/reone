3f9d98bd2071ef81f118bb955c922ee76942d48b
seedhartha <seedhartha.github@gmail.com>
2022-06-11 09:05:41 +0700

    Unit test Strings class
    [ci skip]

--- test/resource/strings.cpp
+++ test/resource/strings.cpp
@@ -17,18 +17,67 @@
 
 #include <boost/test/unit_test.hpp>
 
+#include "../../src/common/logutil.h"
+#include "../../src/common/stream/fileoutput.h"
 #include "../../src/resource/strings.h"
 
 using namespace std;
+
+using namespace reone;
+using namespace reone::resource;
+
+namespace fs = boost::filesystem;
 
 BOOST_AUTO_TEST_SUITE(strings)
 
 BOOST_AUTO_TEST_CASE(should_init_talktable_and_get_string_and_sound) {
     // given
 
+    setLogLevel(LogLevel::None);
+
+    auto tmpDirPath = fs::temp_directory_path();
+    tmpDirPath.append("reone_test_strings");
+    fs::create_directory(tmpDirPath);
+
+    auto tlkPath = tmpDirPath;
+    tlkPath.append("dialog.tlk");
+    auto tlk = FileOutputStream(tlkPath, OpenMode::Binary);
+    tlk.write("TLK V3.0");
+    tlk.write("\x00\x00\x00\x00", 4);
+    tlk.write("\x01\x00\x00\x00", 4);
+    tlk.write("\x3c\x00\x00\x00", 4);
+    // String 0 Data
+    tlk.write("\x03\x00\x00\x00", 4);
+    tlk.write("some_sound\x00\x00\x00\x00\x00\x00", 16);
+    tlk.write("\x00\x00\x00\x00", 4);
+    tlk.write("\x00\x00\x00\x00", 4);
+    tlk.write("\x00\x00\x00\x00", 4);
+    tlk.write("\x0d\x00\x00\x00", 4);
+    tlk.write("\x00\x00\x00\x00", 4);
+    // Strings 0 Entry
+    tlk.write("Hello, world!");
+    //
+    tlk.close();
+
+    auto strings = Strings();
+
+    auto expectedText = string("Hello, world!");
+    auto expectedSound = string("some_sound");
+
     // when
 
+    strings.init(tmpDirPath);
+    auto text = strings.get(0);
+    auto sound = strings.getSound(0);
+
     // then
+
+    BOOST_CHECK_EQUAL(expectedText, text);
+    BOOST_CHECK_EQUAL(expectedSound, sound);
+
+    // cleanup
+
+    fs::remove_all(tmpDirPath);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
