7bf1cd527b9a88fcba60e8ab2448635f885258c3
seedhartha <seedhartha.github@gmail.com>
2022-06-12 07:41:36 +0700

    [game] Add placeholder tests for object selection
    [ci skip]

---
+++ test/game/selectioncontroller.cpp
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2020-2022 The reone project contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(selection_controller)
+
+BOOST_AUTO_TEST_CASE(should_set_hovered_target_on_mouse_motion) {
+    // given
+
+    // when
+
+    // then
+}
+
+BOOST_AUTO_TEST_CASE(should_set_selected_target_on_mouse_click) {
+    // given
+
+    // when
+
+    // then
+}
+
+BOOST_AUTO_TEST_CASE(should_reset_selected_target_on_mouse_click_when_no_object_at_mouse_position) {
+    // given
+
+    // when
+
+    // then
+}
+
+BOOST_AUTO_TEST_SUITE_END()
