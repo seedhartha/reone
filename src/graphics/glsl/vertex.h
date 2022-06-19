db3c2edd049b9677d34130cf93bf5ce6055bb892
seedhartha <seedhartha.github@gmail.com>
2022-06-19 11:12:46 +0700

    Fix shader linking on MacOS

--- src/graphics/glsl/vertex.h
+++ src/graphics/glsl/vertex.h
@@ -36,13 +36,19 @@ void main() {
 
 const std::string g_vsClipSpace = R"END(
 layout(location = 0) in vec3 aPosition;
+layout(location = 1) in vec3 aNormal;
 layout(location = 2) in vec2 aUV1;
 
 out vec4 fragPosWorldSpace;
+out vec3 fragNormalWorldSpace;
 out vec2 fragUV1;
 
 void main() {
     fragPosWorldSpace = uModel * vec4(aPosition, 1.0);
+
+    mat3 normalMatrix = transpose(mat3(uModelInv));
+    fragNormalWorldSpace = normalize(normalMatrix * aNormal);
+
     fragUV1 = aUV1;
 
     gl_Position = uProjection * uView * fragPosWorldSpace;
