029d42ece542b5f5358ca187705892c4cf1a43ea
seedhartha <seedhartha.github@gmail.com>
2023-05-13 00:09:10 +0100

    Update copyright year to 2023

--- include/reone/game/action/barkstring.h
+++ include/reone/game/action/barkstring.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2021 The reone project contributors
+ * Copyright (c) 2020-2023 The reone project contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
